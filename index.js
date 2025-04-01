9201d5f25b786164820543f8ef9885caa08ac983
prakharstech <prakhar.srivastava0509@gmail.com>
2025-04-01 23:43:03 +0530

    update2

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const path = require('path');
 const fs = require('fs');
 const { fileLoader } = require('ejs');
 const { log } = require('console');
+const PORT = process.env.PORT || 3000;
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine','ejs');
