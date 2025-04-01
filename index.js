519e57b06316719625093fbf84fc8449593a177d
prakharstech <prakhar.srivastava0509@gmail.com>
2025-04-01 22:44:05 +0530

    Commit

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const fs = require('fs');
 const { fileLoader } = require('ejs');
 const { log } = require('console');
 
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine','ejs');
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
