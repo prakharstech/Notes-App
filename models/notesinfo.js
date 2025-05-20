a064bbc8f635c983314c095ebf02406b36c86b67
prakharstech <154759772+prakharstech@users.noreply.github.com>
2025-05-20 18:17:03 +0530

    ref added

--- models/notesinfo.js
+++ models/notesinfo.js
@@ -4,7 +4,10 @@ const mongoose = require('mongoose');
 const notesSchema = mongoose.Schema({
     title: String,
     details: String,
-    user: mongoose.Schema.Types.ObjectId
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user'
+    }
 })
 
 module.exports = mongoose.model('notesinfo',notesSchema);
